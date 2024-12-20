dbd802fbdd6053fa53468243d37a769f900be7d3
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2024-12-20 10:20:46 +0500

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -33,48 +33,52 @@ const Header = () => {
 
   return (
     <div
-      className={`container flex flex-wrap items-center justify-around py-4 ${
-        darkMode ? "bg-black text-white" : "bg-white text-black"
-      }`}
+      className={`${darkMode ? "bg-black text-white" : "bg-white text-black"}`}
     >
-      <div className="w-[112px] h-[36px]">
-        <img className="w-full h-full" src={logo} alt="" />
-      </div>
-      <ul className="flex w-[250px] flex-wrap justify-between">
-        <li className="flex flex-wrap flex-col items-center cursor-pointer">
-          <img src={active_header1} className="text-[20px] text-primary" />
-          <span className="text-primary">Poster</span>
-        </li>
-        <li className="flex flex-wrap flex-col items-center cursor-pointer">
-          <img src={header2} className="text-[20px] text-[#A1A1A1]" />
-          <span className="text-[#A1A1A1]">Sessions</span>
-        </li>
-        <li className="flex flex-wrap flex-col items-center cursor-pointer">
-          <img src={header3} className="text-[20px] text-[#A1A1A1]" />
-          <span className="text-[#A1A1A1]">Tickets</span>
-        </li>
-        <li className="flex flex-wrap flex-col items-center cursor-pointer">
-          <img src={header4} className="text-[20px] text-[#A1A1A1]" />
-          <span className="text-[#A1A1A1]">Search</span>
-        </li>
-      </ul>
-      <div className="flex gap-4 items-center">
-        <select className="h-full bg-slate-900 px-3 py-2 rounded-md text-white">
-          {lang.map((item) => (
-            <option key={item.value} value={item.value}>
-              {item.label}
-            </option>
-          ))}
-        </select>
-        <button className="w-[180px] h-[56px] bg-red-700 text-white py-3 rounded-[12px]">
-          Login
-        </button>
-        <button
-          onClick={toggleDarkMode}
-          className={`w-[56px] h-[56px] flex items-center justify-center rounded-full bg-gray-600`}
-        >
-          {darkMode ? "☀️" : "🌙"}
-        </button>
+      <div
+        className={`container flex flex-wrap items-center justify-around py-4 ${
+          darkMode ? "bg-black text-white" : "bg-white text-black"
+        }`}
+      >
+        <div className="w-[112px] h-[36px]">
+          <img className="w-full h-full" src={logo} alt="" />
+        </div>
+        <ul className="flex w-[250px] flex-wrap justify-between">
+          <li className="flex flex-wrap flex-col items-center cursor-pointer">
+            <img src={active_header1} className="text-[20px] text-primary" />
+            <span className="text-primary">Poster</span>
+          </li>
+          <li className="flex flex-wrap flex-col items-center cursor-pointer">
+            <img src={header2} className="text-[20px] text-[#A1A1A1]" />
+            <span className="text-[#A1A1A1]">Sessions</span>
+          </li>
+          <li className="flex flex-wrap flex-col items-center cursor-pointer">
+            <img src={header3} className="text-[20px] text-[#A1A1A1]" />
+            <span className="text-[#A1A1A1]">Tickets</span>
+          </li>
+          <li className="flex flex-wrap flex-col items-center cursor-pointer">
+            <img src={header4} className="text-[20px] text-[#A1A1A1]" />
+            <span className="text-[#A1A1A1]">Search</span>
+          </li>
+        </ul>
+        <div className="flex gap-4 items-center">
+          <select className="h-full bg-slate-900 px-3 py-2 rounded-md text-white">
+            {lang.map((item) => (
+              <option key={item.value} value={item.value}>
+                {item.label}
+              </option>
+            ))}
+          </select>
+          <button className="w-[180px] h-[56px] bg-red-700 text-white py-3 rounded-[12px]">
+            Login
+          </button>
+          <button
+            onClick={toggleDarkMode}
+            className={`w-[56px] h-[56px] flex items-center justify-center rounded-full bg-gray-600`}
+          >
+            {darkMode ? "☀️" : "🌙"}
+          </button>
+        </div>
       </div>
     </div>
   );
