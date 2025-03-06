f49d6641fef0b02764aab3c9d3e3ab273ab3cbf2
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2025-03-06 09:36:49 +0500

    fixed images

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/favicon.ico",
+            src: "/biletick-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/favicon.ico",
+            src: "/biletick-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
