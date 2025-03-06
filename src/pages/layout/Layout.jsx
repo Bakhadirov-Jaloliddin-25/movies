add96fae572a8d5c5b12d1302d98f25fcbebca18
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2025-03-06 07:54:30 +0500

    fixed logo

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import Header from "../../components/header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/footer/Footer";
-import { Helmet } from "react-helmet";
 
 const Layout = () => {
   return (
     <>
-      <Helmet>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </Helmet>
       <Header />
       <main>
         <Outlet />
