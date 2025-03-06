c4b8107beed12eb08000b8db72fd0430e54e69d5
Bakhadirov-Jaloliddin-25 <jboxodirov4@gmail.com>
2025-03-06 09:21:58 +0500

    fixed mobile version

--- vite.config.js
+++ vite.config.js
@@ -12,18 +12,18 @@ export default defineConfig({
         enabled: false,
       },
       manifest: {
-        name: "Car Wash",
-        short_name: "Car Wash",
-        description: "Car Wash App",
-        theme_color: "#ffffff",
+        name: "Movies",
+        short_name: "Movies",
+        description: "Movies App",
+        theme_color: "#000000",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/favicon.ico",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "/favicon.ico",
             sizes: "512x512",
             type: "image/png",
           },
